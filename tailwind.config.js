2ddd4a17f3758ad19bbfaa24982b51d98f5f4898
achul123 <thetheatregaming@gmail.com>
2025-12-08 18:22:43 +0100

    chore: upgrade to tailwindcss v4 and simplify CLI

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/**/*.ejs", "./plugins/**/**/*.ejs"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")],
-};
